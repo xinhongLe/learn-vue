f5c383ec52b2886e45a59527120537988b8d5411
hong <1207392850@qq.com>
2021-03-02 14:24:04 +0800

    冒泡排序

--- src/pages/learn/learn-base/02/Index.vue
+++ src/pages/learn/learn-base/02/Index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     看 console
+   <div>
+     <el-button type="primary" @click="sortMethod(arr)">冒泡排序</el-button>
+   </div>
   </div>
 </template>
 
@@ -12,7 +15,36 @@ import './01-simple'
 // import './04-deal-table-sum.js'
 
 export default {
-  name: 'Index'
+  name: 'Index',
+  data() {
+    return {
+      arr: [110, 22, 55, 66, 33, 88, 44]
+    }
+  },
+  methods: {
+    handleRouter() {
+      this.$router.push({name: 'learn-watch'})
+    },
+    sortMethod(arr) {
+      for(let i=0; i< arr.length; i++){
+        for(let j=0; j< arr.length -1 - i; j++){
+          if(arr[j] > arr[j+1]){
+            let temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+          }
+        }
+      }
+      console.log(arr, 'ok')
+      return arr
+    }
+  },
+  beforeRouteLeave(to, from, next){
+    to.meta.keepalive = true
+    console.log(to, from, 'opop')
+    next()
+
+  }
 }
 </script>
 
